ef9424ed6c86e93465f0d9fdd5b2790149df34dd
Kevin Jacobs <frevin8r@gmail.com>
2018-05-25 12:11:42 -0700

    rendered app jsx to page

---
+++ client/src/index.jsx
@@ -0,0 +1,9 @@
+//import react libraries & components
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App.jsx';
+
+//render react app to DOM
+
+ReactDOM.render(<App />, document.getElementById("app"));
